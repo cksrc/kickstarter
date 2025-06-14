6deb5e3b6e3af6a843c65f6af2e16de0b5165d07
ck <1712301+cksrc@users.noreply.github.com>
2025-06-14 10:28:07 +0400

    structure enrichment and angular updates

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
 # main.py
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
+# Include routers
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World"}  # main.py
